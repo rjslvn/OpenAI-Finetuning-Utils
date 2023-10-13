182646370de827ba77352298b70a43d0309581a4
Raj <8602178+rjslvn@users.noreply.github.com>
2023-10-13 00:27:42 -0700

    Update start_fine_tuning.sh

--- start_fine_tuning.sh
+++ start_fine_tuning.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
 
-# Initialize virtual environment and install dependencies
+# Advanced Filepinecone Utils Script
+
+echo "🚀 Starting Filepinecone Utils..."
+
+# Initialize environment and paths
+PDF_PATH="data/pdfs/MYGPT.pdf"
+WORD2VEC_PATH="models/googlenews-vectors-negative300.bin"
+EMBEDDINGS_PATH="output/embeddings.json"
+
+# Environment setup
 echo "Setting up environment..."
 if [[ ! -d "venv" ]]; then
   python3 -m venv venv
   source venv/bin/activate
-  pip install transformers beautifulsoup4 tqdm
+  pip install gensim sentence_transformers PyPDF2 tqdm
 else
   source venv/bin/activate
 fi
 
-# Prompt to continue
-read -n 1 -s -r -p "Press any key to continue to data extraction..."
+# Extract text from PDF
+echo "📄 Extracting text from PDFs..."
+pdf_text=$(pdftotext $PDF_PATH -)
+echo "Extraction complete!"
 
-# Extract data from conversations.html
-echo -e "\n\nExtracting data from conversations.html..."
-python ./scripts/extract_from_html.py --input ./data/raw/conversations.html --output ./data/raw/chat_history.txt
+# Generate Word2Vec Embeddings
+echo "🧠 Generating Word2Vec Embeddings..."
+if [ ! -f $WORD2VEC_PATH ]; then
+    echo 'Word2Vec model not found. Downloading...'
+    wget -P models/ -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
+    gunzip models/GoogleNews-vectors-negative300.bin.gz
+fi
 
-# Display progress using tqdm
-python -c "from tqdm import tqdm; import time; [time.sleep(0.01) for _ in tqdm(range(100), desc='Extracting')]"
+python - <<END
+import os
+from gensim.models import KeyedVectors
+pdf_text = '''$pdf_text'''
+model = KeyedVectors.load_word2vec_format("$WORD2VEC_PATH", binary=True)
+words = pdf_text.split()
+embeddings = {}
+for word in words:
+    try:
+        embeddings[word] = model[word].tolist()
+    except:
+        pass
+with open("$EMBEDDINGS_PATH", 'w') as f:
+    json.dump(embeddings, f)
+END
+echo "Embeddings generation complete!"
 
-# Prompt to continue
-read -n 1 -s -r -p "Press any key to continue to data tokenization..."
+# Generate SentenceTransformer Embeddings and save to Pinecone
+echo "🔍 Generating SentenceTransformer Embeddings and saving to Pinecone..."
+python - <<END
+from sentence_transformers import SentenceTransformer
+import json
+model = SentenceTransformer('all-MiniLM-L6-v2')
+with open("data/json/conversations.json", 'r') as f:
+    data = json.load(f)
+embeddings = model.encode([item['text'] for item in data])
+for item, embedding in zip(data, embeddings):
+    item['embedding'] = embedding.tolist()
+with open("output/pinecone_upsert.json", 'w') as f:
+    json.dump(data, f)
+END
+echo "SentenceTransformer embeddings saved successfully!"
 
-# Tokenize extracted data
-echo -e "\n\nTokenizing data..."
-python ./scripts/tokenize_data.py --input ./data/raw/chat_history.txt --output ./data/tokenized/chat_data.jsonl
+# Placeholder for additional functionalities
+echo "🤖 Running main script for query and response generation..."
+# Add any other functionalities or scripts here
+echo "Placeholder for main functionality"
 
-# Display progress using tqdm
-python -c "from tqdm import tqdm; import time; [time.sleep(0.01) for _ in tqdm(range(100), desc='Tokenizing')]"
+# Completion message
+echo "🎉 All tasks completed successfully!"
 
-# Placeholder for further steps like fine-tuning or evaluation
-
-echo -e "\n\nProcess complete! Remember to deactivate the virtual environment when done."
+# Deactivate the virtual environment
+echo "Deactivating environment..."
+deactivate
